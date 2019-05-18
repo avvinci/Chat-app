0aeb16dcec296b4930872de68dbb89e6da8204d4
avvinci <vinciabhinav7@gmail.com>
2019-05-18 15:54:47 +0530

    added basic chat functionalit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http) ; 
 
 
 app.get('/', function(req,res){
-    res.sendfile('index.html') ; 
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection',function(socket){
     console.log('user connected') ; 
     socket.on('chat message', function(msg){
-        io.emit('chat message',msg);
+    console.log('chat message : '+ msg) ; 
+
+//         io.emit('chat message',msg);
     });
     socket.on('disconnect',function(){
         console.log('user disconnnected') ;
