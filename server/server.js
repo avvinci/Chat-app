6e6296877157cd28f3fc690aaabec6b72c287317
avvinci <vinciabhinav7@gmail.com>
2019-05-18 16:03:13 +0530

    socket io chat basic done

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ io.on('connection',function(socket){
     console.log('user connected') ; 
     socket.on('chat message', function(msg){
     console.log('chat message : '+ msg) ; 
-
-//         io.emit('chat message',msg);
+    io.emit('chat message', msg);
     });
     socket.on('disconnect',function(){
         console.log('user disconnnected') ;
