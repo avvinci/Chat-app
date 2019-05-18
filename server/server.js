5cb40f678d231585288ed5206157e00b903e404a
avvinci <vinciabhinav7@gmail.com>
2019-05-18 17:51:48 +0530

    username added

--- server/server.js
+++ server/server.js
@@ -10,14 +10,17 @@ app.get('/', function(req,res){
 io.on('connection',function(socket){
     console.log('user connected') ; 
     socket.on('chat message', function(msg){
-    console.log('chat message : '+ msg) ; 
+
+    console.log('chat by : '+ msg.id) ; 
     io.emit('chat message', msg);
     });
     socket.on('disconnect',function(){
+        io.emit('disconnected','a user disconnected');
         console.log('user disconnnected') ;
     });
 
 });
+// io.on('di')
 
 http.listen(3000,function(){
     console.log('listening on *: 3000');
