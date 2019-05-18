8a55d05cb96ce834d43ab56618cae09fcea3726f
avvinci <vinciabhinav7@gmail.com>
2019-05-18 21:32:20 +0530

    user disconnected corrected

--- server/server.js
+++ server/server.js
@@ -2,23 +2,24 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http) ; 
 
-let name = "" ; 
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection',function(socket){
+    let name = "" ; 
     console.log('user connected') ; 
     socket.on('chat message', function(msg){
     console.log('chat by : '+ msg.id) ; 
-    name = msg.id ; 
     io.emit('chat message', msg);
     });
-    // socket.on('new user', function(msg){
-    //     name  = msg ; 
-    // });
-    socket.on('disconnect',function(){
+
+    socket.on('new user', function(msg){
+        name  = msg ; 
+    });
+
+    socket.on('disconnect', function(){
         io.emit('disconnected', name + ' has disconnected');
         console.log('user disconnnected') ;
     });
